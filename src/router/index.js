88180e108def21c7536fa025e3c60240eb3e61cd
wuzhentao1230 <wztsnn@163.com>
2019-05-09 00:37:44 +0800

    tiiji

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import login from '@/components/login'
 import home from '@/components/home'
 import db from '@/utils/localstorage'
+import request from '@/utils/request'
 
 Vue.use(Router)
 let constRouter = [
@@ -25,7 +26,7 @@ let router = new Router({
 })
 
 const whiteList = ['/login']
-
+let asyncRouter
 // 导航守卫，渲染动态路由
 router.beforeEach((to, from, next) => {
   if (whiteList.indexOf(to.path) !== -1) {
@@ -33,11 +34,57 @@ router.beforeEach((to, from, next) => {
   }
   let token = db.get('USER_TOKEN')
   let user = db.get('USER')
+  let userRouter = get('USER_ROUTER')
   if (token.length && user) {
-    next()
+    if (!asyncRouter) {
+      if (!userRouter) {
+        request.get(`menu/${user.username}`).then((res) => {
+          asyncRouter = res.data
+          save('USER_ROUTER', asyncRouter)
+          go(to, next)
+        })
+      } else {
+        asyncRouter = userRouter
+        go(to, next)
+      }
+    } else {
+      next()
+    }
   } else {
     next('/login')
   }
 })
 
+function go (to, next) {
+  asyncRouter = filterAsyncRouter(asyncRouter)
+  router.addRoutes(asyncRouter)
+  next({...to, replace: true})
+}
+
+function save (name, data) {
+  localStorage.setItem(name, JSON.stringify(data))
+}
+
+function get (name) {
+  return JSON.parse(localStorage.getItem(name))
+}
+function filterAsyncRouter (routes) {
+  return routes.filter((route) => {
+    let component = route.component
+    if (component) {
+      route.component = view(component)
+      if (route.children && route.children.length) {
+        route.children = filterAsyncRouter(route.children)
+      }
+      return true
+    }
+  })
+}
+function view (path) {
+  return function (resolve) {
+    import(`@/views/${path}.vue`).then(mod => {
+      resolve(mod)
+    })
+  }
+}
 export default router
